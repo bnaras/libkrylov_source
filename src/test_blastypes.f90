7327ff9a71ec51a89e14b95de0c038b5c673e43f
Samuel Bekoe <sbekoe@uci.edu>
2020-06-05 12:12:40 -0700

    WIP: unit testing for ggesvd and ggegqr

--- src/test_blastypes.f90
+++ src/test_blastypes.f90
@@ -44,6 +44,7 @@ program test_blastypes
   type(base) :: z_array4(n,n)
   type(base) :: z_vector1(n)
   type(base) :: z_vector2(n)
+  type(base) :: tau(n)
 
 ! integer for loops
   integer(kind_integer) :: j1,j2 = 0
@@ -836,14 +837,14 @@ program test_blastypes
 !! write ggetrf and operation
   write(unit=funit,fmt=*) 'test ggetrf', &
   &', calculate for LU decomposition of a matrix'
-!! call gpotrf to on test input onto test output
+!! call ggetrf to on test input onto test output
   call ggetrf(n,n,z_array2,n,ipiv,ierr)
 !! set logical check = .false.
   check = .false.
-!! checking ierr value to see if gpotrf terminated with an error
+!! checking ierr value to see if ggetrf terminated with an error
 !! test if ierr is not equal to 0
   if (ierr.ne.0) then
-!! if true, write gpotrf failed and write the ierr value
+!! if true, write ggetrf failed and write the ierr value
     write(unit=funit,fmt=*) 'ggetrf failed, ierr=', ierr
 !! set ierr to 0
     ierr = 0
@@ -894,7 +895,7 @@ program test_blastypes
 !! write an explanation of the function using formula
   write(unit=funit,fmt=*) 'printing'&
   &',type(base) z_array1 = z_array2^(-1) * z_array2' 
-!! call gtrsm to solve for matrix multiplication of an inverse matrix
+!! call ggetrs to solve for matrix multiplication of an inverse matrix
 !! with left side operation 
 !! on test input onto test output
   call ggetrs('n',n,n,z_array2,n,ipiv,z_array1,n,ierr)
@@ -943,6 +944,153 @@ program test_blastypes
       write(unit=funit,fmt=*) 'tested subroutine ggetrs'
       print *, 'tested subroutine ggetrs'
     end if
+  end if
+
+!!! test ggesvd
+!! using a do loop to fill in the test input matrix
+!! z_array1 
+  z_array1 = real(0,kind=kind_float)
+  do j1 = 1, n
+    z_array1(j1,j1) = real(j1,kind=kind_float)
+  end do
+!! set original r_test and r_ref to real number 0
+  r_test = real(0,kind=kind_float)
+  r_ref = real(0,kind=kind_float)
+!! write ggesvd and operation
+  write(unit=funit,fmt=*) 'test ggesvd', &
+  &', calculate the singular value decomposition of a matrix'
+!! call ggesvd to on test input onto test output
+  call ggesvd('a','a',n,n,z_array1,3,x_array1,z_array2,3,z_array3,3,ierr)
+!! set logical check = .false.
+  check = .false.
+!! checking ierr value to see if ggesvd terminated with an error
+!! test if ierr is not equal to 0
+  if (ierr.ne.0) then
+!! if true, write ggesvd failed and write the ierr value
+    write(unit=funit,fmt=*) 'ggesvd failed, ierr=', ierr
+!! set ierr to 0
+    ierr = 0
+  else
+!! if false, write ggesvd runs
+    write(unit=funit,fmt=*) 'ggesvd runs'
+!! write an explanation of the funtion
+    write(unit=funit,fmt=*) 'type(base) solution for SV decomposition' 
+!! write explanation to the output file
+    write(unit=funit,fmt=*) 'z_array1 should be equal to', &
+  & ' a diagonal matrix with 1,2,3 on diagonal'
+!! write start to check each element in both test array and reference  
+    write(unit=funit,fmt=*) 'test ggesvd, for each element'
+!! using do loops to assign z_array2 to r_test element by element
+!! and r_ref as a diagonal matrix with 1,2,3 on the diagonal
+    do j2 = 1, n 
+      do j1 = 1,n
+        r_test = z_array1(j1,j2)
+        if (j1.eq.j2) then
+          r_ref = real(j2,kind=kind_float)
+        else
+          r_ref = real(0,kind=kind_float) 
+        end if
+!! test the absolute difference of r_test and r_ref 
+!! whether it is greater than machine precision defined by constant eps
+        if (abs(r_test-r_ref).gt.eps) then
+!! if true, write the test failed and the position of element failed
+          write(unit=funit,fmt=*) 'failed for element', j1, j2
+!! set logical check = .true.
+          check = .true.
+        end if
+      end do
+    end do  
+!! write the explanation of function and result
+    write(unit=funit,fmt=*) 'real(kind_float) diagonal matrix S = x_array1'
+    write(unit=funit,fmt=*) 'This should be 1,2,3'
+!! write start to check each element
+!! in both test array and reference
+    write(unit=funit,fmt=*) 'test eigenvalues, for each element'
+!! assign a reference array (real(kind_float))
+!! by operations on real(kind_float) numbers
+!! using a do loop to fill in the matrix for the reference
+    do j1 = 1, n
+      r_test = x_array1(j1)
+      r_ref = real(j1,kind=kind_float)
+!! using a do loop to take the absolute difference
+!! between the elements in test array
+!! and elements in reference value
+!! test if difference is greater than machine precision
+!! (defined by the constant eps)
+      if (abs(r_test-r_ref).gt.eps) then
+!! if true, write the test failed 
+!! and the position of the element that failed
+        write(unit=funit,fmt=*) 'failed for elements', j1
+!! set logical check = .true.
+        check = .true.
+      end if
+    end do
+!! check value of logical check
+    if (check) then
+!! write subroutine ggesvd failed to output and output file
+      write(unit=funit,fmt=*) 'subroutine ggesvd failed'  
+      print *, 'subroutine ggesvd failed'
+    else
+!! write subroutine ggesvd succeeded to output file
+      write(unit=funit,fmt=*) 'tested subroutine ggesvd'
+      print *, 'tested subroutine ggesvd'
+    end if
+  end if
+
+!!! test ggeqrf
+!! using a do loop to fill in the test input matrix
+!! z_array1 
+  z_array1 = real(0,kind=kind_float)
+  do j1 = 1, n
+    z_array2(j1,j1) = real(j1,kind=kind_float)
+  end do
+!! set original r_test and r_ref to real number 0
+  r_test = real(0,kind=kind_float)
+  r_ref = real(0,kind=kind_float)
+!! write ggeqrf and operation
+  write(unit=funit,fmt=*) 'test ggeqrf', &
+  &', calculate for Q of QR decomposition'
+!! call ggeqrf to on test input onto test output
+  call ggeqrf(n,n,z_array1,n,tau,ierr)
+!! set logical check = .false.
+  check = .false.
+!! checking ierr value to see if ggeqrf terminated with an error
+!! test if ierr is not equal to 0
+  if (ierr.ne.0) then
+!! if true, write ggeqrf failed and write the ierr value
+    write(unit=funit,fmt=*) 'ggeqrf failed, ierr=', ierr
+!! set ierr to 0
+    ierr = 0
+  else
+!! if false, write ggeqrf runs
+    write(unit=funit,fmt=*) 'ggeqrf runs'
+!! write an explanation of the funtion
+    write(unit=funit,fmt=*) 'type(base) solution for QR decomposition' 
+!! write explanation to the output file
+    write(unit=funit,fmt=*) 'z_array1 should be equal to', &
+  & ' a diagonal matrix with 1,2,3 on diagonal'
+!! write start to check each element in both test array and reference  
+    write(unit=funit,fmt=*) 'test ggeqrf, for each element'
+!! using do loops to assign z_array1 to r_test element by element
+!! and r_ref as a diagonal matrix with 1,2,3 on the diagonal
+    do j2 = 1, n 
+      do j1 = 1,n
+        r_test = z_array1(j1,j2)
+        if (j1.eq.j2) then
+          r_ref = real(j2,kind=kind_float)
+        else
+          r_ref = real(0,kind=kind_float) 
+        end if
+!! test the absolute difference of r_test and r_ref 
+!! whether it is greater than machine precision defined by constant eps
+        if (abs(r_test-r_ref).gt.eps) then
+!! if true, write the test failed and the position of element failed
+          write(unit=funit,fmt=*) 'failed for element', j1, j2
+!! set logical check = .true.
+          check = .true.
+        end if
+      end do
+    end do  
   end if
 
 
