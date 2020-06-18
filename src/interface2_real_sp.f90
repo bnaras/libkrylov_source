5f7b6f5279df5e00234c38c7a073f2e9820d2fad
Ziyue Shen <ziyues4@riogrande.ps.uci.edu>
2020-06-18 16:41:35 -0700

    WIP: compile polymorphism for real_sp

--- src/interface2_real_sp.f90
+++ src/interface2_real_sp.f90
@@ -14,27 +14,27 @@ module libkrylovinterface2
 ! Modules and Global Varaibles
 !--------------------------------------------------------------------
   use libkrylovinterface_real_dp , only: &
-  &   quicksort_stl_float => quicksort_stl_float_rdp, &
-  &   lkl_s_elec_gas => lkl_s_elec_gas_rdp, &
-  &   lkl_start_elec_gas => lkl_start_elec_gas_rdp, &
-  &   lkl_g_unit_vec => lkl_g_unit_vec_rdp, &
-  &   lkl_guess_unit_vec => lkl_guess_unit_vec_rdp, &
-  &   lkl_pc_all => lkl_pc_all_rdp, &
-  &   lkl_precon_all => lkl_precon_all_rdp, &
-  &   lkl_pc_none => lkl_pc_none_rdp, &
-  &   lkl_precon_none => lkl_precon_none_rdp, &
-  &   lkl_pc_approx => lkl_pc_approx_rdp, &
-  &   lkl_precon_approx => lkl_precon_approx_rdp, &
-  &   lkl_pc_davidson => lkl_pc_davidson_rdp, &
-  &   lkl_precon_davidson => lkl_precon_davidson_rdp, &
-  &   lkl_pc_sleijpen => lkl_pc_sleijpen_rdp, &
-  &   lkl_precon_sleijpen => lkl_precon_sleijpen_rdp, &
-  &   lkl_mta_all => lkl_mta_all_rdp, &
-  &   lkl_maket_a_all => lkl_maket_a_all_rdp, &
-  &   lkl_mtb_all => lkl_mtb_all_rdp, &
-  &   lkl_maket_b_all => lkl_maket_b_all_rdp, &
-  &   lkl_mtc_all => lkl_mtc_all_rdp, &
-  &   lkl_maket_c_all => lkl_maket_c_all_rdp
+  &   quicksort_stl_float => quicksort_stl_float_rsp, &
+  &   lkl_s_elec_gas => lkl_s_elec_gas_rsp, &
+  &   lkl_start_elec_gas => lkl_start_elec_gas_rsp, &
+  &   lkl_g_unit_vec => lkl_g_unit_vec_rsp, &
+  &   lkl_guess_unit_vec => lkl_guess_unit_vec_rsp, &
+  &   lkl_pc_all => lkl_pc_all_rsp, &
+  &   lkl_precon_all => lkl_precon_all_rsp, &
+  &   lkl_pc_none => lkl_pc_none_rsp, &
+  &   lkl_precon_none => lkl_precon_none_rsp, &
+  &   lkl_pc_approx => lkl_pc_approx_rsp, &
+  &   lkl_precon_approx => lkl_precon_approx_rsp, &
+  &   lkl_pc_davidson => lkl_pc_davidson_rsp, &
+  &   lkl_precon_davidson => lkl_precon_davidson_rsp, &
+  &   lkl_pc_sleijpen => lkl_pc_sleijpen_rsp, &
+  &   lkl_precon_sleijpen => lkl_precon_sleijpen_rsp, &
+  &   lkl_mta_all => lkl_mta_all_rsp, &
+  &   lkl_maket_a_all => lkl_maket_a_all_rsp, &
+  &   lkl_mtb_all => lkl_mtb_all_rsp, &
+  &   lkl_maket_b_all => lkl_maket_b_all_rsp, &
+  &   lkl_mtc_all => lkl_mtc_all_rsp, &
+  &   lkl_maket_c_all => lkl_maket_c_all_rsp
 !--------------------------------------------------------------------
 ! Implicit none
 !--------------------------------------------------------------------
