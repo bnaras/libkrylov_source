20dd4cd465184dbac5f1ac55077c4206c3337cda
Ziyue Shen <ziyues4@riogrande.ps.uci.edu>
2020-06-26 13:53:19 -0700

    WIP: fixed restart_b and restart_c for real dp

--- test/real_dp_ref/restart_b/test_script.sh
+++ test/real_dp_ref/restart_b/test_script.sh
@@ -26,7 +26,7 @@ echo 'restart levels' >> ../testing.summary
 echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_k.out' >> ../testing.summary
 touch kill.libkrylov
-echo '4' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_k.out
+../../../src/real_dp_test/driver1b_real_dp -irestart 4 > real_dp_b_restart_k.out
 echo '' >> ../testing.summary
 rm kill.libkrylov
 mv real_dp_b_restart_0.out real_dp_b_restart_0.out.old 2>/dev/null
@@ -34,7 +34,7 @@ echo 'generating save file for' >> ../testing.summary
 echo 'restart level 1' >> ../testing.summary
 echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_0.out' >> ../testing.summary
-echo '1' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_0.out
+../../../src/real_dp_test/driver1b_real_dp -irestart 1 > real_dp_b_restart_0.out
 echo '' >> ../testing.summary
 rm real_dp_1b_lagr.json
 rm real_dp_1b_vecs.json
@@ -42,7 +42,7 @@ mv real_dp_b_restart_1.out real_dp_b_restart_1.out.old 2>/dev/null
 echo 'restart level 1' >> ../testing.summary
 echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_1.out' >> ../testing.summary
-echo '1' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_1.out
+../../../src/real_dp_test/driver1b_real_dp > real_dp_b_restart_1.out
 mv real_dp_1b_lagr.json real_dp_1b_1_lagr.json
 mv real_dp_1b_vecs.json real_dp_1b_1_vecs.json
 grep 'Converged' real_dp_b_restart_1.out >> ../testing.summary
@@ -57,14 +57,14 @@ echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_k.out' >> ../testing.summary
 touch kill.libkrylov
 rm *.save
-echo '4' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_k.out
+../../../src/real_dp_test/driver1b_real_dp -irestart 4 > real_dp_b_restart_k.out
 echo '' >> ../testing.summary
 rm kill.libkrylov
 mv real_dp_b_restart_2.out real_dp_b_restart_2.out.old 2>/dev/null
 echo 'restart level 2' >> ../testing.summary
 echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_2.out' >> ../testing.summary
-echo '2' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_2.out
+../../../src/real_dp_test/driver1b_real_dp -irestart 2 > real_dp_b_restart_2.out
 mv real_dp_1b_lagr.json real_dp_1b_2_lagr.json
 mv real_dp_1b_vecs.json real_dp_1b_2_vecs.json
 grep 'Converged' real_dp_b_restart_2.out >> ../testing.summary
@@ -79,14 +79,14 @@ echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_k.out' >> ../testing.summary
 touch kill.libkrylov
 rm *.save
-echo '4' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_k.out
+../../../src/real_dp_test/driver1b_real_dp -irestart 4 > real_dp_b_restart_k.out
 echo '' >> ../testing.summary
 rm kill.libkrylov
 mv real_dp_b_restart_3.out real_dp_b_restart_3.out.old 2>/dev/null
 echo 'restart level 3' >> ../testing.summary
 echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_3.out' >> ../testing.summary
-echo '3' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_3.out
+../../../src/real_dp_test/driver1b_real_dp -irestart 3 > real_dp_b_restart_3.out
 mv real_dp_1b_lagr.json real_dp_1b_3_lagr.json
 mv real_dp_1b_vecs.json real_dp_1b_3_vecs.json
 grep 'Converged' real_dp_b_restart_3.out >> ../testing.summary
@@ -101,14 +101,14 @@ echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_k.out' >> ../testing.summary
 touch kill.libkrylov
 rm *.save
-echo '4' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_k.out
+../../../src/real_dp_test/driver1b_real_dp -irestart 4 > real_dp_b_restart_k.out
 echo '' >> ../testing.summary
 rm kill.libkrylov
 mv real_dp_b_restart_4.out real_dp_b_restart_4.out.old 2>/dev/null
 echo 'restart level 4' >> ../testing.summary
 echo 'test output in' >> ../testing.summary
 echo 'test/real_dp_testing.results/restart_b/real_dp_b_restart_4.out' >> ../testing.summary
-echo '4' | ../../../src/real_dp_test/test_restart_b_real_dp > real_dp_b_restart_4.out
+../../../src/real_dp_test/driver1b_real_dp -irestart 4 > real_dp_b_restart_4.out
 mv real_dp_1b_lagr.json real_dp_1b_4_lagr.json
 mv real_dp_1b_vecs.json real_dp_1b_4_vecs.json
 grep 'Converged' real_dp_b_restart_4.out >> ../testing.summary
